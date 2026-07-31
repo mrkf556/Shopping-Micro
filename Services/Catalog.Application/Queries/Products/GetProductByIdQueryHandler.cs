57c4c83d754f86900b44e40a38b49c1e31f89d9d
MohammadReza <MohammadReza@DEVELOPER>
2026-07-31 19:44:16 +0330

    get product by id handler

---
+++ Services/Catalog.Application/Queries/Products/GetProductByIdQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Application.Queries.Products
+{
+    public class GetProductByIdQuery : IRequest<ProductResponse>
+    {
+        public string Id { get; set; }
+        public GetProductByIdQuery(string id)
+        {
+            Id = id;
+        }
+
+    }
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductResponse>
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _productRepository;
+        public GetProductByIdQueryHandler(IMapper mapper, IProductRepository productRepository)
+        {
+            _mapper = mapper;
+            _productRepository = productRepository;
+        }
+        public async Task<ProductResponse> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _productRepository.GetProductsById(request.Id);
+            if (result == null)
+            {
+                throw new Exception($"Product Id Not Found:{request.Id}");
+            }
+            return _mapper.Map<ProductResponse>(result);
+
+        }
+    }
+}
