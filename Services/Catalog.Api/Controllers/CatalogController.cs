cf23fb193a3451f9adbde0875ff3c7fc217861b0
MohammadReza <MohammadReza@DEVELOPER>
2026-08-02 21:21:09 +0330

    category controller add actions

--- Services/Catalog.Api/Controllers/CatalogController.cs
+++ Services/Catalog.Api/Controllers/CatalogController.cs
@@ -1,4 +1,7 @@
-﻿using Catalog.Application.Queries.Products;
+﻿using Catalog.Application.Handlers.Products;
+using Catalog.Application.Queries.Brands;
+using Catalog.Application.Queries.Products;
+using Catalog.Application.Queries.Types;
 using Catalog.Application.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Http;
@@ -19,10 +22,56 @@ namespace Catalog.Api.Controllers
             return Ok(await _mediator.Send(new GetProductByIdQuery(id), cancellation));
         }
         [HttpGet("{name}")]
-        public async Task<IActionResult> GetProductsByName(string name, CancellationToken cancellation)
+        public async Task<ActionResult<ProductResponse>> GetProductsByName(string name, CancellationToken cancellation)
         {
             return Ok(await _mediator.Send(new GetProductByNameQuery(name), cancellation));
         }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductResponse>>> GetAllProducts(CancellationToken cancellation)
+        {
+
+
+            return Ok(await _mediator.Send(new GetAllProductsQuery(), cancellation));
+
+        }
+        [HttpGet("brand")]
+        public async Task<ActionResult<IEnumerable<BrandResponse>>> GetProductByBrand(CancellationToken cancellation)
+        {
+            return Ok(await _mediator.Send(new GetAllProductBrandQuery(), cancellation));
+
+
+        }
+        [HttpGet("type")]
+        public async Task<ActionResult<IEnumerable<TypeResponse>>> GetProductByType(CancellationToken cancellation)
+        {
+            return Ok(await _mediator.Send(new GetAllProductTypeQuery(), cancellation));
+
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductResponse>> CreateProduct([FromBody] CreateProductCommand createProduct, CancellationToken cancellation)
+        {
+            return Ok(await _mediator.Send(createProduct, cancellation));
+
+        }
+        [HttpPut]
+        public async Task<ActionResult<bool>> UpdateProduct([FromBody] UpdateProductCommand updateProduct, CancellationToken cancellation)
+        {
+            return Ok(await _mediator.Send(updateProduct, cancellation));
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(string id, CancellationToken cancellation)
+        {
+            return Ok(await _mediator.Send(new DeleteProductCommand(id), cancellation));
+
+        }
+
+
+
+
+
 
     }
 }
